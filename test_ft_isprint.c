259147d89253678663ae24cfb07156d45b445d95
Louis <louis.s.xu@gmail.com>
2021-12-29 01:21:54 +1030

    Add tests

---
+++ test_ft_isprint.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "ft_isprint.c"
+#include <ctype.h>
+
+int main(void)
+{
+	unsigned char tests[] = {'a', 'b', 'z', '1', '5', '9', ' ', 'A', 'B', 'Z', '!', '[', '{', '\0'};
+	int i = 0;
+	int ft_result;
+	int inbuilt_result;
+
+	while (tests[i])
+	{
+		printf("Testing: %c ", tests[i]);
+		ft_result = ft_isprint(tests[i]);
+		inbuilt_result = isprint(tests[i]);
+		if (ft_result == inbuilt_result)
+		{
+			printf("- same - ");
+		}
+		else
+		{
+			printf("- DIFFERENT - ");
+		}
+		printf("ft: %d ", ft_result);
+		printf("inbuilt: %d\n", inbuilt_result);
+		i++;
+	}
+}
+
