bccf9a0c0e4db5fa372d8234a5f77fb3d5414181
Louis <louis.s.xu@gmail.com>
2022-01-10 22:31:14 +1030

    fix: Cast pointers to allow indexing. And import library to access size_t type

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,24 +6,30 @@
 /*   By: lxu <lxu@student.42adel.org.au>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 16:54:09 by lxu               #+#    #+#             */
-/*   Updated: 2022/01/10 17:10:33 by lxu              ###   ########.fr       */
+/*   Updated: 2022/01/10 21:20:14 by lxu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	size_t	i;
+	char	*dst_ptr;
+	char	*src_ptr;
 
-	if (dst == src)
+	dst_ptr = dst;
+	src_ptr = (char *)src;
+	if (dst_ptr == src_ptr)
 	{
 		return (dst);
 	}
-	if (src - dst > 0)
+	if (src_ptr - dst_ptr > 0)
 	{
 		i = 0;
 		while (i < len)
 		{
-			dst[i] = src[i];
+			dst_ptr[i] = src_ptr[i];
 			i++;
 		}
 	}
@@ -32,7 +38,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 		i = len - 1;
 		while (i >= 0)
 		{
-			dst[i] = src[i];
+			dst_ptr[i] = src_ptr[i];
 			i--;
 		}
 	}
