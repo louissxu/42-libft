00fb296eac481c7e3f41eb8135294f7a036e66cb
Louis <louis.s.xu@gmail.com>
2022-01-10 22:32:11 +1030

    fix: Cast pointer to allow indexing. And import library to access size_t type

--- ft_memset.c
+++ ft_memset.c
@@ -6,18 +6,22 @@
 /*   By: lxu <lxu@student.42adel.org.au>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 16:54:11 by lxu               #+#    #+#             */
-/*   Updated: 2022/01/10 17:10:18 by lxu              ###   ########.fr       */
+/*   Updated: 2022/01/10 21:18:55 by lxu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 void	*ft_memset(void *b, int c, size_t len)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*b_ptr;
 
+	b_ptr = b;
 	i = 0;
 	while (i < len)
 	{
-		b[i] = (unsigned char)c;
+		b_ptr[i] = (unsigned char)c;
 		i++;
 	}
 	return (b);
