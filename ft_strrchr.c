04bdbf68b688dab522f9d99e61acb9aa72e30ff0
Louis <louis.s.xu@gmail.com>
2022-01-10 22:46:08 +1030

    fix: Cast pointer to allow modification and change 0 -> NULL for clarity

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,18 +6,20 @@
 /*   By: lxu <lxu@student.42adel.org.au>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 16:54:55 by lxu               #+#    #+#             */
-/*   Updated: 2022/01/10 17:43:44 by lxu              ###   ########.fr       */
+/*   Updated: 2022/01/10 21:30:07 by lxu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdlib.h>
 
 char	*ft_strrchr(const char *s, int c)
 {
 	char	*result;
 	char	*ptr;
 
-	ptr = s;
+	ptr = (char *)s;
 	c = (char)c;
-	result = 0;
+	result = NULL;
 	while (*ptr)
 	{
 		if (*ptr == c)
