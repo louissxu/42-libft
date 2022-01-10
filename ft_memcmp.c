5bae92c7acdb3ab697c95cf1a025b20772928713
Louis <louis.s.xu@gmail.com>
2022-01-10 22:22:58 +1030

    fix: Cast pointers to allow indexing. Add include to add size_t type

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,22 +6,28 @@
 /*   By: lxu <lxu@student.42adel.org.au>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 16:54:06 by lxu               #+#    #+#             */
-/*   Updated: 2022/01/10 17:09:13 by lxu              ###   ########.fr       */
+/*   Updated: 2022/01/10 20:51:50 by lxu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t	i;
+	char	*s1_ptr;
+	char	*s2_ptr;
 
+	s1_ptr = (char *)s1;
+	s2_ptr = (char *)s2;
 	i = 0;
-	while (s1[i] == s2[i] && i < n)
+	while (s1_ptr[i] == s2_ptr[i] && i < n)
 	{
 		i++;
 	}
 	if (i < n)
 	{
-		return (s1[i] - s2[i]);
+		return (s1_ptr[i] - s2_ptr[i]);
 	}
 	return (0);
 }
