af2be3e0015edff6bbf9ba259171ebb2e33cca11
Louis <louis.s.xu@gmail.com>
2022-01-10 16:52:36 +1030

    Fix: Missing semicolon

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -26,7 +26,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 		while (haystack[i + j] && needle[j] && \
 				haystack[i + j] == needle[j] && i + j < len)
 		{
-			j++
+			j++;
 		}
 		if (!needle[j] && i + j <= len)
 		{
