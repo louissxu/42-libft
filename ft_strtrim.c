7022f2d3d10917af3d3e14d26eff96a5a59ec335
Louis <louis.s.xu@gmail.com>
2022-01-10 23:00:10 +1030

    fix: Fix function name and parameter type

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,13 +6,13 @@
 /*   By: lxu <lxu@student.42adel.org.au>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 16:54:56 by lxu               #+#    #+#             */
-/*   Updated: 2022/01/10 17:45:17 by lxu              ###   ########.fr       */
+/*   Updated: 2022/01/10 21:32:42 by lxu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	priv_ft_inset(char c, char *set)
+static int	priv_ft_inset(char c, char const *set)
 {
 	while (*set)
 	{
@@ -34,7 +34,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	start = 0;
 	end = ft_strlen(s1);
-	while (s1[start] && ft_inset(s1[start], set))
+	while (s1[start] && priv_ft_inset(s1[start], set))
 		start++;
 	while (end > start && priv_ft_inset(s1[end - 1], set))
 		end--;
