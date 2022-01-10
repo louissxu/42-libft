a91971b204cf9139295639f6624aa282bc78fe13
Louis <louis.s.xu@gmail.com>
2022-01-11 03:28:13 +1030

    fix: Handle unsigned char

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: lxu <lxu@student.42adel.org.au>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 16:54:06 by lxu               #+#    #+#             */
-/*   Updated: 2022/01/10 20:51:50 by lxu              ###   ########.fr       */
+/*   Updated: 2022/01/10 23:51:06 by lxu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t	i;
-	char	*s1_ptr;
-	char	*s2_ptr;
+	size_t			i;
+	unsigned char	*s1_ptr;
+	unsigned char	*s2_ptr;
 
-	s1_ptr = (char *)s1;
-	s2_ptr = (char *)s2;
+	s1_ptr = (unsigned char *)s1;
+	s2_ptr = (unsigned char *)s2;
 	i = 0;
 	while (s1_ptr[i] == s2_ptr[i] && i < n)
 	{
