d331b8b4390241c91f66213d684cab1813bd9f09
Louis <louis.s.xu@gmail.com>
2022-01-11 03:31:15 +1030

    fix: Wrong variable name

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: lxu <lxu@student.42adel.org.au>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 16:54:48 by lxu               #+#    #+#             */
-/*   Updated: 2022/01/10 17:43:26 by lxu              ###   ########.fr       */
+/*   Updated: 2022/01/11 01:32:03 by lxu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	i = 0;
 	while (i < len)
 	{
-		result[i] = f(i, result[i]);
+		result[i] = f(i, s[i]);
 		i++;
 	}
 	result[i] = '\0';
