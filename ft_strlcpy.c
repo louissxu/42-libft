d0355f8c7ce454d3428c910df3aee3418b2b990f
Louis <louis.s.xu@gmail.com>
2022-01-11 03:30:53 +1030

    fix: Handle case where dstsize = 0

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: lxu <lxu@student.42adel.org.au>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 16:54:44 by lxu               #+#    #+#             */
-/*   Updated: 2022/01/10 21:25:46 by lxu              ###   ########.fr       */
+/*   Updated: 2022/01/11 03:21:32 by lxu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 
+	if (dstsize == 0)
+	{
+		return (ft_strlen(src));
+	}
 	i = 0;
 	while (src[i] && i < dstsize - 1)
 	{
