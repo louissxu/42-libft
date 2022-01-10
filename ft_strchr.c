2e8850181fe75f92ee7163daac99fd3b2bde9edb
Louis <louis.s.xu@gmail.com>
2022-01-10 22:34:28 +1030

    fix: Cast pointer to allow indexing. Return NULL rather than 0 for clarity

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,15 +6,17 @@
 /*   By: lxu <lxu@student.42adel.org.au>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 16:54:37 by lxu               #+#    #+#             */
-/*   Updated: 2022/01/10 16:54:38 by lxu              ###   ########.fr       */
+/*   Updated: 2022/01/10 21:23:00 by lxu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdlib.h>
 
 char	*ft_strchr(const char *s, int c)
 {
 	char	*result;
 
-	result = s;
+	result = (char *)s;
 	c = (char)c;
 	while (*result && *result != c)
 	{
@@ -24,5 +26,5 @@ char	*ft_strchr(const char *s, int c)
 	{
 		return (result);
 	}
-	return (0);
+	return (NULL);
 }
