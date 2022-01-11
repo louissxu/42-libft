0b9f11f6f66e82230b9fbf73b9da915e751f84c2
Louis <louis.s.xu@gmail.com>
2022-01-11 16:46:47 +1030

    Add test

---
+++ test_ft_substr.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "ft_substr.c"
+#include <string.h>
+
+int main(void)
+{
+	char *s = ft_substr("tripouille", 100, 1);
+	printf("The result string is: %s\n", s);
+	printf("The result pass/fail is: %d\n", strcmp(s, ""));
+}
