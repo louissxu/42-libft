84b3704e60225995c895709816f46d978c23ce5b
louissxu <louis.s.xu@gmail.com>
2022-06-26 19:33:57 +0930

    feat(core): Add ft_lstindex function

--- includes/libft.h
+++ includes/libft.h
@@ -65,6 +65,7 @@ t_list	*ft_lstlast(t_list *lst);
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 t_list	*ft_lstnew(void *content);
 int		ft_lstsize(t_list *lst);
+t_list	*ft_lstindex(t_list *lst, size_t index);
 
 /*
  * MEMORY
